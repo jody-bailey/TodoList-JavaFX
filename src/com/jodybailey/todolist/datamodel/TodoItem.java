ee1f2785541230ee321f8a9dfec236c738a68d3d
jody-bailey <jodybailey93@gmail.com>
2018-03-07 17:03:16 -0600

    initial commit

---
+++ src/com/jodybailey/todolist/datamodel/TodoItem.java
@@ -0,0 +1,48 @@
+package com.jodybailey.todolist.datamodel;
+
+import java.time.LocalDate;
+
+/**
+ * Created by jbailey on 3/7/2018.
+ */
+public class TodoItem {
+
+    private String shortDescription;
+    private String details;
+    private LocalDate deadline;
+
+    public TodoItem(String shortDescription, String details, LocalDate deadline) {
+        this.shortDescription = shortDescription;
+        this.details = details;
+        this.deadline = deadline;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String toString() {
+        return shortDescription;
+    }
+}
